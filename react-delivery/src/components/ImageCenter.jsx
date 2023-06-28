b6b1653a04c588d561aaeefa996d76127fd82c9f
Alejandro Montero <alejandro.montero@correounivalle.edu.co>
2023-06-28 13:38:17 -0500

    Agregando bootstrap para estilos dinamicos

--- react-delivery/src/components/ImageCenter.jsx
+++ react-delivery/src/components/ImageCenter.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ImageCenter = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
-      <img src="https://www.advotics.com/wp-content/uploads/2022/02/surat-jalan-01-1-4-1024x656.png" alt="Imagen" />
+      <img className='img-fluid' src="https://www.advotics.com/wp-content/uploads/2022/02/surat-jalan-01-1-4-1024x656.png" alt="Imagen" />
     </div>
   );
 };
--- react-delivery/src/components/Trabajadores.jsx
+++ react-delivery/src/components/Trabajadores.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Trabajadores.css'
 import Header from './Header'
 class Trabajadores extends Component{
@@ -8,11 +9,13 @@ class Trabajadores extends Component{
     return(
       <>
       <Header/>
-      <div className="menu">
+      <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <div className="rounded rounded-lg menu" style={{ width: '600px', height: '300px', backgroundColor: 'rgb(105, 150, 168' }}>
         <ul>
           <li><a className= "boton-enlace" href="/verTrabajadores">VER TRABAJADORES</a></li>
           <li><a className= "boton-enlace" href="/EditarTrabajadores">CREAR / ELIMINAR TRABAJADORES</a></li>
         </ul>
+      </div>
       </div>
       </>
     )
